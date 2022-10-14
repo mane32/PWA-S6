e6b0990dc20a1b51adc63637e7681206b97c864f
Vanne Vega <manelp3214@gmail.com>
2022-10-13 20:35:34 -0500

    limpiar cache dinamico

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
 // const CACHE_NAME = 'cache-1';
-const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_STATIC_NAME = 'static-v2';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
+function limpiarCache(cacheName, numeroItems) { 
+    caches.open(cacheName)
+    .then(cache => {
 
+        return cache.keys()
+            .then(keys => {
+
+                if (keys.length > numeroItems) {
+                    cache.delete(keys[0])
+                        .then(limpiarCache(cacheName, numeroItems));
+                }
+            });
+
+
+    });
+}
 
 self.addEventListener('install', e => {
 
@@ -42,6 +57,7 @@ self.addEventListener('fetch', e => {
                     caches.open(CACHE_DYNAMIC_NAME)
                     .then(cache => {
                         cache.put(e.request, newResp);
+                        limpiarCache(CACHE_DYNAMIC_NAME, 5);
                           });
 
                     return newResp.clone();
