689fb9fa1045c91a13c49b3fe4dd77bd66400e3d
Vanne Vega <manelp3214@gmail.com>
2022-10-13 20:03:08 -0500

    cache with network

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-
+const CACHE_NAME = 'cache-1';
 
 
 self.addEventListener('install', e => {
 
-    const cacheProm = caches.open('cache-1')
+    const cacheProm = caches.open(CACHE_NAME)
     .then(cache => {
 
      return  cache.addAll([
@@ -20,6 +20,28 @@ self.addEventListener('install', e => {
 
 
 self.addEventListener('fetch', e => {
+    // 2- Cache with Network Fallback
+     const respuesta =  caches.match(e.request)
+       .then(res => {
+              if(res) return res;
+                 //No existe el archivo 
+                 //tengo que ir a la web
+                 console.log('No existe ', e.request.url);
+                 return fetch(e.request)
+                 .then(newResp => {
+
+                    caches.open(CACHE_NAME).then(cache => {
+                        cache.put(e.request, newResp);
+                          });
+
+                    return newResp.clone();
+
+                 });
+
+       });
+       e.repondWith(respuesta);
+
+
   //1- cache only 
-  e.respondWith(caches.match (e.request));
+ // e.respondWith(caches.match (e.request));
 });
